dc26eb9bf969f0ed045eb6c28e92005a4968c353
pjchristie12 <30814003+pjchristie12@users.noreply.github.com>
2019-09-10 20:33:40 -0500

    Update mainBot.php

--- mainBot.php
+++ mainBot.php
@@ -6,7 +6,7 @@ include('./httpful.phar');
 include('./functions.php');
 
 // this is found at http://dev.groupme.com/bots
-$bot_token = "the main bot id";
+$bot_token = "9b2f61bb37b82b138168d367c7";
 
 include('./index.php');
 
